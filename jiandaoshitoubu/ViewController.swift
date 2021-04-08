06b9e3ad83532380d3bc3b024447aa79081fbcab
chenzhizs <chenzhi@qq.com>
2021-04-08 11:12:36 +0900

    暂时做好剪刀石头布模板

--- jiandaoshitoubu/ViewController.swift
+++ jiandaoshitoubu/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var 现在的用户选的剪刀石头布 = 1
+    var 现在的电脑选的剪刀石头布 = 1
+    
 
+    @IBOutlet weak var 布: UIButton!
+    @IBOutlet weak var 剪刀: UIButton!
+    @IBOutlet weak var 石头: UIButton!
+    @IBOutlet weak var 电脑选的: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func 布的函数(_ sender: Any) {
+        现在的用户选的剪刀石头布 = 1
+        
+        
+        现在的电脑选的剪刀石头布 = Int.random(in: 1...3)
+        
+        电脑选的.image = UIImage(named: "\(现在的电脑选的剪刀石头布)")
+    }
+    
+    @IBAction func 剪刀的函数(_ sender: Any) {
+        现在的用户选的剪刀石头布 = 2
+        
+        
+        现在的电脑选的剪刀石头布 = Int.random(in: 1...3)
+        
+        电脑选的.image = UIImage(named: "\(现在的电脑选的剪刀石头布)")
+    }
+    
+    @IBAction func 石头的函数(_ sender: Any) {
+        现在的用户选的剪刀石头布 = 3
+        
+        
+        现在的电脑选的剪刀石头布 = Int.random(in: 1...3)
+        
+        电脑选的.image = UIImage(named: "\(现在的电脑选的剪刀石头布)")
+    }
 }
 
